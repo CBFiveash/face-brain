d58c225261e359fc9899fd4d507eccec03065269
Charli Fiveash <charli.fiveash@gmail.com>
2022-07-12 13:46:03 -0700

    add db functionality

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,22 +1,10 @@
 import React from "react";
 
-<<<<<<< HEAD
 const Rank = ({ name, entries }) => {
     return (
       <div>
         <div className='white f3'>
           {`${name}, your current entry count is...`}
-=======
-const  Rank = ({ name, entries }) => {
-    return (
-        <div>
-            <div className="white f3">
-                {`${name}, your current entry count is... `}
-            </div>
-            <div className="white f1">
-                { entries }
-            </div>
->>>>>>> 399ec4ac3210389ccbc42233309c95ff7f30f146
         </div>
         <div className='white f1'>
           {entries}
